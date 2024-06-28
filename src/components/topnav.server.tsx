52ffac3f04d0484cf3e45e48ec5e10cd37166c05
AltF4irl <altf4irl@gmail.com>
2024-06-28 12:46:30 +0300

    Added multiple uploads

--- src/components/topnav.server.tsx
+++ src/components/topnav.server.tsx
@@ -9,7 +9,7 @@ export default function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b bg-neutral-950 p-4 text-xl font-semibold">
       <div>Skill Issue</div>
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
         </SignedOut>
