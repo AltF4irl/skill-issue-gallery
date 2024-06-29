d853abcbcdb8e76e2a19dce642112eb803f37cb6
AltF4irl <altf4irl@gmail.com>
2024-06-29 22:24:34 +0300

    Fix: Corrected get query

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,9 +1,18 @@
 import "server-only";
 
 import { db } from "./db/index";
+import { auth } from "@clerk/nextjs/server";
 
-export const getImages = async () => {
+export const getUserImages = async () => {
+
+    const user = auth();
+
+    if (!user.userId) {
+        throw new Error("Not Authorrized");
+    }
+
   const images = await db.query.posts.findMany({
+    where: (model, {eq}) => eq(model.userId, user.userId),
     orderBy: (model, { desc }) => desc(model.id),
   });
   return images;
