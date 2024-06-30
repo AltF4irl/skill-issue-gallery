60ac6cad1166ba72836fd3c6920822f9048a59e9
AltF4irl <altf4irl@gmail.com>
2024-06-30 21:09:44 +0300

    Mod: Tweaked Ui of modal

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,19 +1,21 @@
-import Image from "next/image";
 import { getImage } from "~/server/queries";
 
-export default async function FullPageImaggeView({photoId} : {photoId: string,}) {
-    if (isNaN(Number(photoId))) throw new Error("Invalid Image id");
-    const image = await getImage(Number(photoId));
-  
-    return (
-      <div>
-          <Image
-            src={image.url}
-            alt={image.name}
-            width={480}
-            height={240}
-            style={{ objectFit: "contain" }}
-          />
+export default async function FullPageImaggeView({
+  photoId,
+}: {
+  photoId: string;
+}) {
+  if (isNaN(Number(photoId))) throw new Error("Invalid Image id");
+  const image = await getImage(Number(photoId));
+
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex-shrink flex justify-center items-center">
+        <img src={image.url} alt={image.name} />
       </div>
-    );
-  }
+      <div className="flex w-48 flex-col flex-shrink-0 border-l border-neutral-800">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
+}
--- src/components/topnav.server.tsx
+++ src/components/topnav.server.tsx
@@ -8,12 +8,12 @@ import Link from "next/link";
 export default function TopNav() {
   const router = useRouter();
   return (
-    <nav className="flex md:flex-col items-center justify-between border-b bg-neutral-950 p-4 text-xl font-semibold md:h-screen">
+    <nav className="flex md:flex-col items-center justify-between border-b md:border-r border-neutral-800 md:border-b-0 bg-neutral-950 p-4 text-xl font-semibold md:h-screen">
       <Link
         className="mb-2 flex text-center justify-start"
         href="/"
       >
-        <div>Skil Issue</div>
+        <div>Skill Issue</div>
       </Link>
       <div className="flex flex-row md:flex-col items-center ">
         <SignedOut>
