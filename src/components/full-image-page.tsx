c583453341f734d2a1db434a2b474977131bd23d
AltF4irl <altf4irl@gmail.com>
2024-07-01 01:40:43 +0300

    Mod: modal ui

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -9,12 +9,22 @@ export default async function FullPageImaggeView({
   const image = await getImage(Number(photoId));
 
   return (
-    <div className="flex h-full w-full min-w-0">
-      <div className="flex-shrink flex justify-center items-center">
-        <img src={image.url} alt={image.name} />
+    <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
+      <div className="flex-shrink flex-grow">
+        <img src={image.url} className="object-contain" alt={image.name} />
       </div>
-      <div className="flex w-48 flex-col flex-shrink-0 border-l border-neutral-800">
-        <div className="text-xl font-bold">{image.name}</div>
+      <div className="flex h-full w-56 flex-shrink-0 flex-col border-l border-neutral-800">
+        <div className="border-b p-2 text-center text-xl border-neutral-800">{image.name}</div>
+
+        <div className="p-2">
+          <div>Uploaded By:</div>
+          <div className="overflow-clip">{image.userId}</div>
+        </div>
+
+        <div className="p-2">
+          <div>Created On:</div>
+          <div>{image.createdAt.toLocaleDateString()}</div>
+        </div>
       </div>
     </div>
   );
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([]);
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+export default clerkMiddleware((auth, request) => {
+  if (isProtectedRoute(request)) auth().protect();
 });
 
 export const config = {
