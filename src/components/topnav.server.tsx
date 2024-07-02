1143515962e18e7bd4474da0e46924fba90b0411
AltF4irl <altf4irl@gmail.com>
2024-07-02 10:01:00 +0300

    Added: shadcn setup

--- src/components/topnav.server.tsx
+++ src/components/topnav.server.tsx
@@ -6,7 +6,7 @@ import { SimpleUploadButton } from "./simple-upload-button";
 
 export default function TopNav() {
   return (
-    <nav className="flex items-center justify-between border-b border-neutral-800 p-4 text-xl font-semibold md:h-screen md:flex-col md:border-b-0 md:border-r">
+    <nav className="flex items-center justify-between border-b bg-neutral-950 border-neutral-800 p-4 text-xl font-semibold md:h-screen md:flex-col md:border-b-0 md:border-r">
       <Link className="flex justify-start text-center" href="/">
         <div className="hidden md:!block">S<br />k<br />i<br />l<br />l<br /><br /> I<br />s<br />s<br />u<br />e</div>
         <div className="md:hidden">Skill Issue</div>
